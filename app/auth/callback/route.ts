6091c05e2169fff8a2ff322599f9ec5781634c11
Matt <director@acidgambit.com>
2023-06-16 13:41:15 -0400

    change auth provider to google

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -2,7 +2,7 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server';
 import type { Database } from '@/types_db'
 
 export async function GET(request: NextRequest) {
--- types_db.ts
+++ types_db.ts
@@ -223,11 +223,9 @@ export interface Database {
         | "incomplete_expired"
         | "past_due"
         | "unpaid"
-        | "paused"
     }
     CompositeTypes: {
       [_ in never]: never
     }
   }
 }
-
