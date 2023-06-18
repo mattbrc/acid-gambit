fff48efc5ba72bd86e033071502b531a902bdc4a
Matt <director@acidgambit.com>
2023-06-18 11:00:47 -0400

    update nutrition

---
+++ app/nutrition/page.tsx
@@ -0,0 +1,27 @@
+import { getSession, getSubscription } from '@/app/supabase-server';
+import { DashboardHeader } from '@/components/header';
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export default async function Wod() {
+  const supabase = createServerComponentClient({
+    cookies
+  });
+
+  const subscription = await getSubscription();
+  const session = await getSession();
+  const userId = session?.user.id;
+  const { data: profile } = await supabase
+    .from('profiles')
+    .select('username, full_name')
+    .eq('id', userId);
+
+  return (
+    <div>
+      <div className="pb-2">
+        <DashboardHeader heading="Nutrition" text="View Your Workouts" />
+      </div>
+    </div>
+  );
+}
